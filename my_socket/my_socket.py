ccc36421c98214143f35044fda2bc6a3b473cbdf
chris <104324755+cjurgens17@users.noreply.github.com>
2024-07-27 10:35:24 -0400

    Changed directory and file name - Socket handler is responding to client

--- my_socket/my_socket.py
+++ my_socket/my_socket.py
@@ -3,10 +3,13 @@
 import asyncio
 import json
 import os
-import secrets
 import signal
-
+import secrets
 import websockets
+
+""""
+Broadcast for all subscribers -> websocket.broadcast(subscribers, json.dumps(some event))
+"""
 
 
 generic_poll_state = {
@@ -14,8 +17,11 @@ generic_poll_state = {
     "global_question_score" : None,
     "current_question": None,
     "all_questions" : [],
-    "polling_allowed": False
+    "polling_allowed": False,
+    "is_started" : False
 }
+
+JOIN_KEYS =  []
 
 
 #Sending an error back to the client
@@ -30,21 +36,20 @@ async def error(websocket, message):
     }
     await websocket.send(json.dumps(generic_poll_event))
 
-#Client joins -> update them with the latest state
-async def replay(websocket):
+#Client joins -> update them with the latest state -> need to grab some incoming id to replay back to specific client
+async def replay(websocket, attendee, id=None):
     """
     Send Most Recent State
 
     Client has to handle the event
 
     """
-    for attendee in generic_poll_state['attendees']:
-        generic_poll_event = {
-            "type": "replay",
-            "attendee": attendee,
-            "current_question" : generic_poll_state['current_question']
-        }
-        await websocket.send(json.dumps(generic_poll_event))
+    generic_poll_event = {
+        "type": "replay",
+        "attendee": attendee,
+        "current_question" : generic_poll_state['current_question']
+    }
+    await websocket.send(json.dumps(generic_poll_event))
 
 
 async def update_global_question_score(websocket, poll_hub,attendee):
@@ -90,16 +95,31 @@ async def update_global_question_score(websocket, poll_hub,attendee):
         await websockets.broadcast(poll_hub, json.dumps(generic_poll_state))
         await websockets.broadcast(attendee, json.dump(poll_decision))
 
-async def start():
-    #Implement to start the websocket
-    return 
+async def generate_join_key(websocket):
+    #Route to hit to send the client a join key for auth
+    
+        #start the socket and turn state to true
+        join_key = secrets.token_urlsafe(12)
+        JOIN_KEYS.append(join_key)
+        await websocket.send(json.dumps({"secret" : join_key}))
 
-async def join(websocket, join_key):
+async def join(websocket):
     """
     Implement for attendees to join
-
+    Multiple joins at the same time -> we await for each message hitting this route
     """
-    return
+    
+    async for message in websocket:
+        message = json.loads(message)
+        print("Client Message", message)
+        if message['secret'] == os.getenv('SECRET_KEY'):
+             generic_poll_state['attendees'].append(message['attendee'])
+        generic_poll_event = {
+            "type": "joined",
+            "message": "You have successfully joined the game"
+        }
+        await websocket.send(json.dumps(generic_poll_event))
+
 
 async def handler(websocket):
     """
@@ -110,29 +130,22 @@ async def handler(websocket):
     }
     """
     # Receive and authenticate new join
-    message = await websocket.recv()
-    join_poll = json.loads(message)
-    client_secret, attendee = join_poll['client_secret'], join_poll['attendee']
-
-    if client_secret ==  os.getenv('SECRET_KEY'):
-        # Add to attendeees and estbalish connection
-        if attendee not in generic_poll_state["attendees"]:
-            generic_poll_state['attendees'].append(attendee)
-            await join(websocket)
-    elif generic_poll_state["attendees"].length <= 0:
-        await start(websocket)
-    else:
-        error(websocket,"Something Went Wrong, contact your administrator")
+    async for message in websocket:
+        message = await websocket.recv()
+        join_poll = json.loads(message)
+        client_secret, attendee = join_poll['client_secret'], join_poll['attendee']
+        print(f"Join Poll {join_poll}")
+        if client_secret ==  os.getenv('SECRET_KEY'):
+            # Add to attendeees and estbalish connection
+            if attendee not in generic_poll_state["attendees"]:
+                generic_poll_state['attendees'].append(attendee)
+                await join(websocket)
+        else:
+            error(websocket,"Something Went Wrong, contact your administrator")
 
 async def main():
-    # Set the stop condition when receiving SIGTERM.
-    loop = asyncio.get_running_loop()
-    stop = loop.create_future()
-    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-
-    port = int(os.environ.get("PORT", "8001"))
-    async with websockets.serve(handler, "", port):
-        await stop
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
 
 
 if __name__ == "__main__":
